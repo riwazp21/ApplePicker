db2c57cad9c74f0652cbf3a29811bd6f99d1852c
Riwaz Poudel <riwazpdel@gmail.com>
2024-02-01 16:55:29 -0600

    first comit

---
+++ Assets/Scripts/Basket.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class Basket : MonoBehaviour
+{
+    [Header("Set Dynamically")]
+    private TMP_Text scoreGT;
+    void Start(){
+        GameObject scoreGO = GameObject.Find("ScoreCounterReal");
+        scoreGT = scoreGO.GetComponent<TMP_Text>();
+        scoreGT.text = "0";
+    }
+    void Update()
+    {
+        UnityEngine.Vector3 mousePos2D = Input.mousePosition;
+
+        mousePos2D.z = -Camera.main.transform.position.z;
+        UnityEngine.Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+        UnityEngine.Vector3 pos = this.transform.position;
+        pos.x = mousePos3D.x;
+        this.transform.position = pos;
+
+    }
+    void OnCollisionEnter(Collision coll){
+        GameObject collideWith = coll.gameObject;
+        if(collideWith.tag=="Apple"){
+            Destroy(collideWith);
+
+            int score = int.Parse(scoreGT.text);
+            score+=100;
+            scoreGT.text = score.ToString();
+            if ( score > HighScore.score){
+                HighScore.score = score;
+            }
+        }
+    }
+}
